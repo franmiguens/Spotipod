4d774c395294ae393cda316e5f562665c5bf92f6
Fran <f.miguens@wustl.edu>
2022-04-29 01:15:30 -0400

    Added youtube functionality. Not done yet

--- YouTubeComm.py
+++ YouTubeComm.py
@@ -1,17 +1,49 @@
+import googleapiclient
 from googleapiclient.discovery import build
 import creds
-
+import youtube_dl
+def my_hook(d):
+    if d['status'] == 'finished':
+        print('done')
 youtube_api_key=creds.get_youtube_api_key()
 
 #We have gotten the tracks, make use of youtube api
 youtube=googleapiclient.discovery.build('youtube','v3',developerKey=youtube_api_key)
 request=youtube.search().list(part='snippet', maxResults=25, q='drugs tai verdes')
 response=request.execute()
-print(response['items'][9]['id'])
+print(response['items'][10]['id'])
 with open("C:/Users/Fran/Desktop/Testing.txt", 'w') as f:
     for y in response['items']:
         if(y['id']['kind'] == 'youtube#video'):
             f.write("https://www.youtube.com/watch?v="+y['id']['videoId']+'\n')
+
+request=youtube.videos().list(part= "contentDetails", id="otyPTI9iMjw")
+response1=request.execute()
+print(response1)
+ydl_opts = {
+    'format' : 'bestaudio/best',
+    'postprocessors' : [{
+        'key' : 'FFmpegExtractAudio',
+        'preferredcodec' : 'm4a',
+        'preferredquality' : '256'
+    }],
+    'progress_hooks' : [my_hook]
+
+}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(['https://www.youtube.com/watch?v=otyPTI9iMjw'])
 #Can make searches. From searches find length closest to song length on spotify. If >30 seconds return in the errors file. put URL into txt
+
+def get_videos(search):
+    request = youtube.search().list(part='snippet', maxResults=25, q=search)
+    response = request.execute()
+    for x in response['items']:
+        if(x['id']['kind'] == 'youtube#video'):
+            return x['id']['videoId']
+
+#def download_audio(url):
+
+
+
 
 
