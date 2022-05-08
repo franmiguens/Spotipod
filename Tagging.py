14b3131e36d7ee9d67413bed139cd0e2c92cecf3
franmiguens <89803582+franmiguens@users.noreply.github.com>
2022-05-08 03:03:00 -0400

    Update Tagging.py
    Added metadata editing functionality

---
+++ Tagging.py
@@ -0,0 +1,11 @@
+from mutagen.mp4 import MP4
+
+
+def metadata(directory, dictionary):
+    f=MP4(directory)
+    f['\xa9nam'] = 'u'+dictionary['song']
+    f['\xa9ART'] = 'u'+dictionary['artist']
+    f['\xa9alb'] = 'u'+dictionary['album_name']
+    f['aART'] = 'u'+dictionary['album_artist']
+    f['desc'] = 'u'+dictionary['song_tracks']+'/'+dictionary['album_tracks']+'  '+dictionary['album_disc']
+    f['\xa9day'] = 'u'+dictionary['year']
